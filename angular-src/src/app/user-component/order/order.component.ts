b16bdef51300d19a3c92ba50f341e2ad2bd19748
anushagg <Anu$ha25>
2018-05-04 20:38:00 -0400

    Order history and Active orders connecting with backend

--- angular-src/src/app/user-component/order/order.component.ts
+++ angular-src/src/app/user-component/order/order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {PlaceOrderService} from "../../services/placeorders/place-order.service";
+import {SaveUserDataService} from "../../services/miscService/save-user-data.service";
+
 
 @Component({
   selector: 'app-order',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  activeOrder = [];
+
+  constructor(private placeOrder: PlaceOrderService,
+              private userDataService: SaveUserDataService,
+              private router: Router) { }
 
   ngOnInit() {
+
+    if(this.userDataService.username != null){
+      const username = {
+        username : this.userDataService.username
+      };
+      this.placeOrder.getActiveOrders(username).subscribe(data=>{
+
+
+
+
+        if (data.success) {
+          console.log(data);
+          data.activeOrder.forEach(i => {
+            console.log(i);
+            // if(i._id != undefined)
+            this.activeOrder.push(i.catalogImage, i.clothName, i.length, i.catalog, i.cost);
+          });
+        }
+
+
+      })
+    }else{
+      this.router.navigate(['/user/login']);
+    }
   }
 
 }
--- angular-src/src/app/user-component/pastorder/pastorder.component.ts
+++ angular-src/src/app/user-component/pastorder/pastorder.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {PlaceOrderService} from "../../services/placeorders/place-order.service";
+import {SaveUserDataService} from "../../services/miscService/save-user-data.service";
+
 
 @Component({
   selector: 'app-pastorder',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastorderComponent implements OnInit {
 
-  constructor() { }
+  pastOrder = [];
+
+
+  constructor(private placeOrder: PlaceOrderService,
+              private userDataService: SaveUserDataService,
+              private router: Router) { }
 
   ngOnInit() {
+
+    if(this.userDataService.username != null){
+      const username = {
+        username : this.userDataService.username
+      };
+      this.placeOrder.getPastOrders(username).subscribe(data=>{
+
+
+
+
+        if (data.success) {
+          console.log(data);
+          data.pastOrder.forEach(i => {
+            console.log(i);
+            // if(i._id != undefined)
+            this.pastOrder.push(i.catalogImage, i.clothName, i.length, i.catalog, i.cost);
+          });
+        }
+
+
+      })
+    }else{
+      this.router.navigate(['/user/login']);
+    }
   }
 
-}
+  }
+
+
