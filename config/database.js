0291fbe622c510484bb7e0cf4d3fe035db547118
chinmayghag <chinmayghag08@gmail.com>
2018-05-08 20:08:00 -0400

    Deployment commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    database : "mongodb://localhost:27017/textilemanagement",
-    // database : "mongodb://root:root@ds117250.mlab.com:17250/textilemanagement",
+    // database : "mongodb://localhost:27017/textilemanagement",
+    database : "mongodb://root:root@ds117250.mlab.com:17250/textilemanagement",
     secret : "yoursecret"
 }
 
